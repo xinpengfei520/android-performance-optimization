4d40a874cda8e80e7ea5ccf41519567bcf4ee8db
xinpengfei <542270559@qq.com>
2019-07-11 11:16:00 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # android-memory-optimized
 Android Memory Optimized Best Practice.
+
+## Reference Link
+
+[提升Android下内存的使用意识和排查能力](https://yq.aliyun.com/articles/225751)
--- app/src/main/java/com/xpf/android/memory/optimized/MainActivity.java
+++ app/src/main/java/com/xpf/android/memory/optimized/MainActivity.java
@@ -2,12 +2,35 @@ package com.xpf.android.memory.optimized;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTest = findViewById(R.id.btnTest);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateImageViews();
+            }
+        });
+    }
+
+    private void generateImageViews() {
+        for (int i = 0; i < 1000; i++) {
+            ImageView imageView = new ImageView(this);
+        }
+        // 手动 gc
+        System.gc();
+        // 强制回收 Finalizer 对象
+        System.runFinalization();
     }
 }
